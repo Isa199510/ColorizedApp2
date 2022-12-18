c260b88a313a4b4896f6b8e350d7014065ea5fb0
Isa <isa.arbuhov@mail.ru>
2022-12-18 23:26:52 +0300

    Initial Commit

---
+++ ColorizedApp2/ColorizedApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp2App.swift
+//  ColorizedApp2
+//
+//  Created by Иса on 18.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
